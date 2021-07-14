20a0a8025e021bbbb682445a95a25d5ed3834b02
BlackcatRs <vithurshan@laposte.net>
2021-07-14 21:28:48 +0200

    large int test

--- large_integer_arithmetic/large_integer.c
+++ large_integer_arithmetic/large_integer.c
@@ -5,28 +5,55 @@ what consume resources
 */
 
 #include <stdio.h>
-#define BASE 10
 
-// 12343
+
+// TO DO : malloc dynamic
+// TO DO : return struct (buff + size)
 int* own_atoi(char const argv[]) {
-	int large_interger[31];
+	static int large_interger[31];
 	int i = 0;
 
 	while (argv[i] != '\0') {
-		large_interger[i] = argv[i++];
+		large_interger[i] = argv[i]; // char to int
+		large_interger[i] -= 48; // modulo
+		i++;
 	}
-	large_interger[i] = '\0';
 
+	return large_interger;
 }
 
 int main(int argc, char const *argv[]) {
-	int* nb = own_atoi(argv[1]);
+	int *nb = own_atoi(argv[1]);
 
-
-	//
-	// for (size_t i = 0; i < 30; i++) {
-	// 	printf("%ld = %d\n", i, nb[i]);
-	// }
+	for (size_t i = 0; i < 6; i++) {
+		printf("%d\n", nb[i]);
+	}
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
